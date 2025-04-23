4bf3ebe6a542ac3d82d072c20f01f80344faaa8a
JCLamprecht <66572608+JCLamprecht@users.noreply.github.com>
2025-04-23 15:08:32 +0200

    Added Note to MainSection

--- src/pages/MainSection.jsx
+++ src/pages/MainSection.jsx
@@ -2,6 +2,9 @@ const MainSection = () => {
   return (
     <section className="section">
       <h1>Main Section</h1>
+      <h1 style={{ color: "black" }}>
+        Note: This site is still under construction
+      </h1>
     </section>
   );
 };
